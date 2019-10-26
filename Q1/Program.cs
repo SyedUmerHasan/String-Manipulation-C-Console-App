e421ed29f64649aad036ec49e7639f15dbc9a15f
umer hasan <umerhasan2010@outlook.com>
2019-10-27 00:45:26 +0500

    second commit

--- Q1/Program.cs
+++ Q1/Program.cs
@@ -10,6 +10,32 @@ namespace Q1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your string  : ");
+            string mystr = Console.ReadLine();
+            string updatedString = "";
+            for(int i = 0; i < mystr.Length;i ++)
+            {
+                int myint = Convert.ToInt32(mystr[i]);
+                if (i < 4)
+                {
+                    updatedString += mystr[i];
+                    continue;
+                }
+
+                if ( myint > 64 && myint < 91  )
+                {
+                    updatedString += mystr[i];
+                }
+                else if (myint > 96 && myint < 123)
+                {
+                    updatedString += Convert.ToChar(mystr[i] - 32);
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("The Update String  : " + updatedString);
+            Console.ReadKey();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+"# String-Manipulation-C-Console-App" 
